aa57b1894ac2cb6aaf9538ef6344dab93d266f4e
yumeme777 <136804501+yumeme777@users.noreply.github.com>
2023-06-16 22:14:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// script.js
+
+// テキストフィールドやテキストエリアの要素を取得
+const yourNameInput = document.getElementById("your-name");
+const yourEmailInput = document.getElementById("your-email");
+const messageTextarea = document.getElementById("message");
+
+// クリック時の動作を設定
+yourNameInput.addEventListener("click", function () {
+  if (this.value === "") {
+    this.value = "あなたの名前を入力してください";
+  }
+});
+
+yourEmailInput.addEventListener("click", function () {
+  if (this.value === "") {
+    this.value = "あなたのメールアドレスを入力してください";
+  }
+});
+
+messageTextarea.addEventListener("click", function () {
+  if (this.value === "") {
+    this.value = "メッセージを入力してください";
+  }
+});
 
